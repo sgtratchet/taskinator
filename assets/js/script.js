9f71973928c332367188e7579d52ba8a9327356d
Devin Ellsworth <devin.ellsworth@power-innovations.com>
2020-05-19 15:04:29 -0600

    new task button now creates new task with user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,23 @@ var taskToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler =function(){
     event.preventDefault();
+    var taskNameInput=document.querySelector("input[name='task-name']").value;
+    console.log(taskNameInput);
+    var taskTypeInput=document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+        //create list item
     var listItemEl = document.createElement('li');
-    listItemEl.className = 'task-item';
-    listItemEl.textContent = 'This is a new task.';
+        listItemEl.className = 'task-item';
+        //create div to hold task info
+    var taskInfoEl= document.createElement("div");
+        //give it a class name
+    taskInfoEl.className ="task-info";
+        //add HTML content to div
+    taskInfoEl.innerHTML ="<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+        //add items to to-do list
     taskToDoEl.appendChild(listItemEl);
-}
+};
 
 formEl.addEventListener("submit", createTaskHandler);
-
