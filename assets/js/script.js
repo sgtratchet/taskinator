a756e8b5c0adf8804254d2ee494f4885fdf006de
Devin Ellsworth <devin.ellsworth@power-innovations.com>
2020-05-21 14:17:04 -0600

    polished drag feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -178,6 +178,8 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
       event.preventDefault();
+      
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 var dropTaskHandler = function(event) {
@@ -196,11 +198,19 @@ var dropTaskHandler = function(event) {
       else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
         }
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
   };
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
